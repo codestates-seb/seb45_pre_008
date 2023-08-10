2a7d8ec24fd8624d32c83951a32390992be67669
nuyhv <volumefd@gmail.com>
2023-08-10 17:46:40 +0900

    ✨Header 초기 구성

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const HeaderComponent = styled.header``;
+
+const Header = () => {
+  return <HeaderComponent>헤더</HeaderComponent>;
+};
+
+export default Header;
