726c863960453dc545c65156329bdd0314ce49a7
nuyhv <volumefd@gmail.com>
2023-08-10 14:48:58 +0900

    ✅ Route 임시 연결

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import GlobalStyle from './assets/styles/GlobalStyle.jsx';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer.jsx';
+import Login from './pages/Login.jsx';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <GlobalStyle />
       <BrowserRouter>
         <Routes>
-          <Route path="/" />
+          <Route path="/" element={<Login />} />
         </Routes>
         <Footer />
       </BrowserRouter>
