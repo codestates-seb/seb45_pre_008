c162becec45fa282b26a8c5b030f6351e1f7f861
MS_btff <whdghks845@gmail.com>
2023-08-11 10:17:11 +0900

    로그인 페이지 ✨

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
 import './App.css';
+// import Login from './pages/Login.jsx';
+import SignUp from './pages/SignUp.jsx';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      {/* <Login /> */}
+      <SignUp />
+    </div>
+  );
 }
 
 export default App;
