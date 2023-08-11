56e549bb0b9266a8ae6b697b78b433092bb8acf6
MS_btff <whdghks845@gmail.com>
2023-08-11 13:04:43 +0900

    로그인 페이지 ✨

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,6 @@ import { Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer.jsx';
 import Header from './components/Header.jsx';
 import Login from './pages/Login.jsx';
-// import Login from './pages/Login.jsx';
 import SignUp from './pages/SignUp.jsx';
 
 function App() {
