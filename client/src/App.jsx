6773e898c9a33aacc206e6a9c6dcf63cc4618f34
LEE YEJIN <dldulwls@gmail.com>
2023-08-11 11:07:06 +0900

    ✨게시글 작성

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Question from './components/Question.jsx';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Question />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Draft.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import { Editor } from 'react-draft-wysiwyg';
+import { EditorState } from 'draft-js';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+
+const Container = styled.div`
+  width: 100%;
+`;
+
+const Draft = () => {
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+  const updateTextDescription = (state) => {
+    setEditorState(state);
+  };
+
+  return (
+    <>
+      <Container>
+        <Editor
+          editorState={editorState}
+          onEditorStateChange={updateTextDescription}
+          editorStyle={{
+            height: '200px',
+            width: '100%',
+            border: '1px solid lightgray',
+          }}
+          toolbar={{
+            options: ['inline', 'blockType', 'list', 'textAlign'], // 필요한 옵션만 남기고 나머지 삭제
+            inline: {
+              options: ['bold', 'italic', 'underline', 'strikethrough'],
+            },
+            blockType: {
+              inDropdown: true,
+              options: ['Normal', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6'],
+            },
+            list: {
+              inDropdown: true,
+              options: ['unordered', 'ordered'],
+            },
+            textAlign: {
+              inDropdown: true,
+              options: ['left', 'center', 'right'],
+            },
+          }}
+        />
+      </Container>
+    </>
+  );
+};
+
+export default Draft;
