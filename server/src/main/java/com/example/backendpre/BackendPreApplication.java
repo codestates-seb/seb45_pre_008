984d388421c3f576d95d9f07df338ecfef5d31c7
JangJeonggyu <129978430+JangJeonggyu@users.noreply.github.com>
2023-08-14 09:37:24 +0900

    Add files via upload

---
+++ server/src/main/java/com/example/backendpre/BackendPreApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendpre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPreApplication.class, args);
+	}
+
+}
