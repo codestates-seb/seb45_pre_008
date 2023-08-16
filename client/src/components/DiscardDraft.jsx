b7139be91bf883125dc21744674922e4149d82b8
LEE YEJIN <dldulwls@gmail.com>
2023-08-16 14:44:48 +0900

    💄discard draft  모달창 css 수정

--- client/src/components/DiscardDraft.jsx
+++ client/src/components/DiscardDraft.jsx
@@ -1,30 +1,58 @@
 import { useState } from 'react';
 import styled from 'styled-components';
+import { Button } from './../assets/styles/QuestionStyle.jsx';
 
-const Button = styled.button`
-  all: unset;
+const DiscardDraftButton = styled(Button)`
   color: rgb(194, 45, 50);
-  padding: 10.4px;
-  font-size: 13px;
+  background-color: #f8f9f9;
   font-weight: 400;
-  border-radius: 6px;
-  cursor: pointer;
+  margin-left: 10px;
   &:hover {
-    background-color: rgb(253, 242, 242);
     color: rgb(172, 37, 42);
+    background-color: rgb(253, 242, 242);
   }
   &:active {
     color: rgb(194, 45, 50);
     background-color: rgb(249, 211, 211);
   }
 `;
-const ModalTitle = styled.div``;
+const AskButton = styled(Button)`
+  background-color: #d0393e;
+  margin-right: 10px;
+  &:hover {
+    background-color: rgb(194, 45, 50);
+  }
+`;
+const CancelButton = styled(Button)`
+  color: #3b4045;
+  background-color: #ffffff;
+  &:hover {
+    color: #3b4045;
+    background-color: #f8f9f9;
+  }
+`;
+const XButton = styled(CancelButton)`
+  padding: 10px;
+  margin: 0;
+  width: 40px;
+  font-size: 20px;
+`;
+const ModalTitle = styled.div`
+  font-size: 27px;
+  font-weight: 600;
+  color: #c22e32;
+`;
+const ModalTopContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 const ModalOverlay = styled.div`
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
+  color: #3b4045;
   background-color: rgba(0, 0, 0, 0.5);
   display: flex;
   justify-content: center;
@@ -33,7 +61,7 @@ const ModalOverlay = styled.div`
 
 const ModalContent = styled.div`
   background-color: white;
-  padding: 20px;
+  padding: 24px;
   border-radius: 8px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
 `;
@@ -54,17 +82,20 @@ export default function DiscardDraft() {
   };
   return (
     <>
-      <Button onClick={openModal}>Discard draft</Button>
+      <DiscardDraftButton onClick={openModal}>Discard draft</DiscardDraftButton>
       {isModalOpen && (
         <ModalOverlay onClick={handleModalClick}>
           <ModalContent>
-            <ModalTitle>Discard question</ModalTitle>
+            <ModalTopContainer>
+              <ModalTitle>Discard question</ModalTitle>
+              <XButton onClick={closeModal}>&times;</XButton>
+            </ModalTopContainer>
             <p>
               Are you sure you want to discard this question? This cannot be
               undone.
             </p>
-            <Button onClick={closeModal}>Discard question</Button>
-            <Button onClick={closeModal}>Cancel</Button>
+            <AskButton>Discard question</AskButton>
+            <CancelButton onClick={closeModal}>Cancel</CancelButton>
           </ModalContent>
         </ModalOverlay>
       )}
